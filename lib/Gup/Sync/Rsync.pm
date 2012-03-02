4c69f1160f137abab2e14a9f86f56ca9ac25c945
R3VoLuT1OneR <r3volut1oner@gmail.com>
2012-03-02 21:04:36 +0200

    Attributes for Rsync method

--- lib/Gup/Sync/Rsync.pm
+++ lib/Gup/Sync/Rsync.pm
@@ -3,6 +3,31 @@ use warnings;
 package Gup::Sync::Rsync;
 
 use Moo;
+use Sub::Quote;
+
+has host => (
+    is       => 'ro',
+    isa      => quote_sub( q{
+        $_[0] =~ /^(?:[A-Za-z0-9_-]|\.)+$/ or die "Improper host: '$_[0]'\n";
+    } ),
+    required => 1,
+);
+
+has user => (
+    is       => 'ro',
+    isa      => quote_sub( q{
+        $_[0] =~ /^(?:[A-Za-z0-9_-]|\.)+$/ or die "Improper user: '$_[0]'\n";
+    } ),
+    required => 1,
+);
+
+has dir => (
+    is       => 'ro',
+    isa      => quote_sub( q{
+        $_[0] =~ /^(?:[A-Za-z0-9_-]|\.|\/)+$/ or die "Improper dir: '$_[0]'\n";
+    } ),
+    required => 1,
+);
 
 sub sync_dir {
     my $self = shift;
@@ -14,7 +39,7 @@ sub sync_dir {
     my $cmd = System::Command->new(
         'rsync',
         '-avc', '--quiet',
-        "$user\@$host:$dir",
+        "$user\@$host:$dir/",'.'
     );
 }
 
