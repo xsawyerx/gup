83dfbbbdee9fb5c853b31105b6defbfd45965ee2
Sawyer X <xsawyerx@cpan.org>
2012-03-03 21:01:24 +0200

    provide host attribute

--- lib/Gup/Role/Syncer.pm
+++ lib/Gup/Role/Syncer.pm
@@ -7,6 +7,14 @@ use Moo::Role;
 
 requires 'sync';
 
+has host => (
+    is       => 'ro',
+    isa      => quote_sub( q{
+        $_[0] =~ /^(?:[A-Za-z0-9_-]|\.)+$/ or die "Improper host: '$_[0]'\n";
+    } ),
+    required => 1,
+);
+
 1;
 
 __END__
