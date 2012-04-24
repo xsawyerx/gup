1ca092acea5ba3d8e8a8754c640a8a8fd3ab31de
Pavel Zhytomirsky <pavelz@netvertise.co.il>
2012-04-24 13:51:42 +0300

    Added ssh path and scp path for Sync Role

--- lib/Gup/Role/Sync.pm
+++ lib/Gup/Role/Sync.pm
@@ -6,16 +6,30 @@ package Gup::Role::Sync;
 use English '-no_match_vars';
 use Moo::Role;
 use Sub::Quote;
+use MooX::Types::MooseLike::Base qw/Str/;
 
 requires 'sync';
 
 with 'Gup::Role::Plugin';
+
+has ssh_path => (
+    is       => 'ro',
+    isa      => Str,
+    default  => quote_sub( q{'/usr/bin/ssh'} ),
+);
+
+has scp_path => (
+    is       => 'ro',
+    isa      => Str,
+    default  => quote_sub( q{'/usr/bin/scp'} ),
+);
 
 has host => (
     is       => 'ro',
     isa      => quote_sub( q{
         $_[0] =~ /^(?:[A-Za-z0-9_-]|\.)+$/ or die "Improper host: '$_[0]'\n";
     } ),
+    predicate => 'has_host',
 );
 
 # these two could be separated at some point to Gup::Role::SyncUser
@@ -35,4 +49,3 @@ sub _build_username { getpwuid $REAL_USER_ID }
 1;
 
 __END__
-
