0cf97c1d95777fb770c1e4f8fa75d133219cdd8f
Pavel R3VoLuT1OneR Zhitomirsky <r3volut1oner@gmail.com>
2012-05-07 02:56:44 +0300

    Remove auth attrbuts from sync role

--- lib/Gup/Role/Sync.pm
+++ lib/Gup/Role/Sync.pm
@@ -24,28 +24,6 @@ has scp_path => (
     default  => quote_sub( q{'/usr/bin/scp'} ),
 );
 
-has host => (
-    is       => 'ro',
-    isa      => quote_sub( q{
-        $_[0] =~ /^(?:[A-Za-z0-9_-]|\.)+$/ or die "Improper host: '$_[0]'\n";
-    } ),
-    predicate => 'has_host',
-);
-
-# these two could be separated at some point to Gup::Role::SyncUser
-# also, they could also be added to Gup::Role::SyncAuthUser / SyncUser
-# one would make these required while others won't
-# this would make web sync possible with optional user/pass authentications
-has username => (
-    is       => 'ro',
-    isa      => quote_sub( q{
-        $_[0] =~ /^(?:[A-Za-z0-9_-]|\.)*$/ or die "Improper user: '$_[0]'\n";
-    } ),
-    builder  => '_build_username',
-);
-
-sub _build_username { getpwuid $REAL_USER_ID }
-
 1;
 
 __END__
